fe806f0c2f0a2938230553891eec22ef08dd9130
Javier Borja <javierborja.2014@gmail.com>
2022-06-28 09:28:35 -0600

    Agregar archivos de proyecto.

---
+++ DB/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaFacturacionWeb.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
