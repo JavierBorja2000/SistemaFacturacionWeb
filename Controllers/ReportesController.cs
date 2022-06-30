430da9864dec820a4752fb71d522b77d58a5ca50
Marcela Mazariegos <Marcela.mazariegos@outlook.com>
2022-06-30 15:57:41 -0600

    reportes_marcela

---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaFacturacionWeb.DB;
+using SistemaFacturacionWeb.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SistemaFacturacionWeb.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly ApplicationDbContext DbContext;
+        static string cadena = "Server=localhost\\SQLEXPRESS;Database=SistemaFacturacion;Trusted_Connection=true;MultipleActiveResultSets=true";
+
+        public ReportesController(ApplicationDbContext context)
+        {
+            DbContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rep_Producto(Reportes reporte)
+        {
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_ReporteProducto", cn);
+                cmd.Parameters.AddWithValue("Fecha1", reporte.Fecha1);
+                cmd.Parameters.AddWithValue("Fecha2", reporte.Fecha2);
+                cmd.Parameters.AddWithValue("Nombre Producto", reporte.Nombre);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                
+            }
+
+                return View();
+            }
+
+        public IActionResult Rep_Cliente(Reportes reporte)
+        {
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_ReporteProducto", cn);
+                cmd.Parameters.AddWithValue("Fecha1", reporte.Fecha1);
+                cmd.Parameters.AddWithValue("Fecha2", reporte.Fecha2);
+                cmd.Parameters.AddWithValue("Nombre Producto", reporte.Nombre);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+            }
+
+            return View();
+        }
+
+        public IActionResult Rep_Factura(Reportes reporte)
+        {
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_ReporteProducto", cn);
+                cmd.Parameters.AddWithValue("Fecha1", reporte.Fecha1);
+                cmd.Parameters.AddWithValue("Fecha2", reporte.Fecha2);
+                
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+            }
+
+            return View();
+        }
+    }
+}
+
+
+
+
+
+   
+
+        
+
+
+    
+
---
+++ Models/Reportes.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaFacturacionWeb.Models
+{
+    public class Reportes
+    {
+
+        [Display(Name = "Fecha Inicial")]
+        [DataType(DataType.Date)]
+        public DateTime? Fecha1 { get; set; }
+
+        [Required(ErrorMessage = "El campo fecha Final no puede ir vacio")]
+        [Display(Name = "Fecha Final")]
+        [DataType(DataType.Date)]
+        public DateTime? Fecha2 { get; set; }
+
+        [Display(Name = "Nombre")]
+        public string? Nombre { get; set; }
+
+    }
+}
