5a91562677b943b5911415c9fa03af9bbf2008f3
Javier Borja <javierborja.2014@gmail.com>
2022-07-01 04:23:51 -0600

    Pantalla de Reportes y modificacion a los botones del proyecto

---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaFacturacionWeb.Controllers
+{
+    public class ReportesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
