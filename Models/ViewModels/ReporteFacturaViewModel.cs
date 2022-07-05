b72c18d2e6371601dcb40cc4077470dde65f2088
Marcela Mazariegos <Marcela.mazariegos@outlook.com>
2022-07-05 09:27:13 -0600

    Estadisticas por Factura

---
+++ Models/ViewModels/ReporteFacturaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SistemaFacturacionWeb.Models.ViewModels
+{
+    public class ReporteFacturaViewModel
+    {
+        [DataType(DataType.Date)]
+        public DateTime? Fecha { get; set; }
+
+        public int Cantidad_facturas { get; set; }
+
+        public float? Total_facturado { get; set; }
+
+        public int Cantidad_productos { get; set; }
+    }
+}
