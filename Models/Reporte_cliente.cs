9ef10414199535618c774fe18f198b0dbaab420a
jdanisosa <danny.sosaa1@gmail.com>
2022-07-05 12:41:34 -0600

    Reporte de ventas por cliente finalizado

---
+++ Models/Reporte_cliente.cs
@@ -0,0 +1,9 @@
+﻿using SistemaFacturacionWeb.Models.ViewModels;
+namespace SistemaFacturacionWeb.Models
+{
+    public class Reporte_cliente
+    {
+        public Reporte2ViewModel Reporte2ViewModel { get; set; }
+        public List<ReporteClienteViewModel>? ResultadosClientes { get; set; }
+    }
+}
